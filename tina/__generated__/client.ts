bff993470c519c1edc467bbac1a1eb0fb4376f3e
hopkinsjmp <hopkinsjmp@gmail.com>
2026-03-01 14:09:48 +0000

    chore: final schema sync and lockfile

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/Users/josephhopkins/crumbsblog/tina/__generated__/.cache/1772373334829', url: 'http://localhost:4001/graphql', token: '0482bd24fa3d53454063c6a37f85bbd4ef92f06a', queries,  });
+export const client = createClient({ cacheDir: '/workspaces/crumbsblog/tina/__generated__/.cache/1772374139736', url: 'http://localhost:4001/graphql', token: '0482bd24fa3d53454063c6a37f85bbd4ef92f06a', queries,  });
 export default client;
   
